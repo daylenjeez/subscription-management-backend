c017549a12efe43cfbc834416d00f5cfa1f85bb2
daylenJeez <stussury@gmail.com>
2025-01-03 14:09:23 +0800

    Refactor subscription DTO and service to enhance validation and type safety
    - Updated CreateSubscriptionSchema in subscription.dto.ts to include optional presetServiceId and improved validation logic for service details.
    - Modified SubscriptionService to use CreateSubscriptionType for create and modify methods, ensuring consistent data handling.
    - Removed deprecated CreateSubscriptionParams type from request.types.ts to streamline type definitions.

--- src/dtos/subscription.dto.ts
+++ src/dtos/subscription.dto.ts
@@ -1,26 +1,34 @@
-import { CURRENCIES, SERVICE_TYPES } from "@/constants/enum";
+import { CURRENCIES } from "@/constants/enum";
 
-import { Type } from "@sinclair/typebox";
+import { type Static, Type } from "@sinclair/typebox";
 
 // 创建订阅的请求结构
 export const CreateSubscriptionSchema = Type.Object({
-	// 服务类型
-	serviceType: Type.Enum(SERVICE_TYPES),
-
-	// 当 serviceType 为 CUSTOM 时的自定义类型
-	customServiceType: Type.Optional(Type.String()),
+	presetServiceId: Type.Optional(Type.String({ format: "uuid" })),
+	...Type.Union([
+		// 当有 presetServiceId 时的验证
+		Type.Object({
+			presetServiceId: Type.String({ format: "uuid" }),
+		}),
+		// 当没有 presetServiceId 时的验证
+		Type.Object({
+			presetServiceId: Type.Null(),
+			serviceName: Type.String(),
+			serviceLogo: Type.String(),
+		}),
+	]).properties,
 
 	// 服务名称
 	serviceName: Type.String(),
 
 	// 订阅周期
 	startDate: Type.String({ format: "date-time" }),
-	endDate: Type.String({ format: "date-time" }),
+	// endDate: Type.String({ format: "date-time" }),
 
 	// 金额信息
 	amount: Type.Number(),
 	currency: Type.Enum(CURRENCIES),
-
-	// 可选备注
-	note: Type.Optional(Type.String()),
 });
+
+// 生成 TypeScript 类型
+export type CreateSubscriptionType = Static<typeof CreateSubscriptionSchema>;
--- src/services/subscription.service.ts
+++ src/services/subscription.service.ts
@@ -1,13 +1,21 @@
+import type { CreateSubscriptionType } from "@/dtos/subscription.dto";
 import type { Subscription } from "@/entities/subscription.entity";
-import type { CreateSubscriptionParams } from "@/types/request.types";
 import type { Repository } from "typeorm";
 
 export class SubscriptionService {
 	constructor(private subscriptionRepo: Repository<Subscription>) {}
 
-	async create(data: CreateSubscriptionParams): Promise<Subscription> {
+	async create(data: CreateSubscriptionType): Promise<Subscription> {
 		const subscription = this.subscriptionRepo.create(data);
 		return this.subscriptionRepo.save(subscription);
+	}
+
+	async modify(
+		id: string,
+		data: CreateSubscriptionType,
+	): Promise<Subscription> {
+		const subscription = await this.findOne(id);
+		return this.subscriptionRepo.save({ ...subscription, ...data });
 	}
 
 	async findAll(): Promise<Subscription[]> {
